faa4f5ab7cb963f7e5134d63c1f2c8fd3f57748c
oranje19 <jasonwintery@gmail.com>
2020-05-10 22:40:04 -0700

    Add to Heroku

--- validation/register.js
+++ validation/register.js
@@ -7,7 +7,7 @@ module.exports = function validateRegisterInput(data) {
     data.handle = validText(data.handle) ? data.handle : '';
     data.email = validText(data.email) ? data.email : '';
     data.password = validText(data.password) ? data.password : '';
-    data.password2 = calidText(data.password2) ? data.password2 : '';
+    data.password2 = validText(data.password2) ? data.password2 : '';
 
     if (!Validator.isLength(data.handle, { min: 2, max: 30 })) {
         errors.handle = 'Handle must be between 2 and 30 characters';
