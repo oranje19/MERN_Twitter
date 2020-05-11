679671a9ed630b629ebb6e09d50e7283470ca1fc
oranje19 <jasonwintery@gmail.com>
2020-05-10 21:57:15 -0700

    Deploy to Heroku

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,7 +1,7 @@
 import * as APIUtil from '../util/session_api_util';
 import jwt_decode from 'jwt-decode';
 
-export const RECEIVE_CURRENT_USER = "RECEIVE_CIRRENT_USER";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -7,7 +7,7 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
         !loggedIn ? (
             <Component {...props} />
         ) : (
-            <Redirect ro="/tweets" />
+            <Redirect to="/tweets" />
         )
     )} />
 );
