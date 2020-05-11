5eb61eb4172b65ce540aa22604f796ff4044c5f7
oranje19 <jasonwintery@gmail.com>
2020-05-11 00:23:12 -0700

    typo

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-// const path = require('path');
+const path = require('path');
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("frontend/build"));
-//     app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
