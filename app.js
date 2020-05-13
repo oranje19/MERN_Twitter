83cb34c42f39d73efe59a159e8e4183a8e4c2480
oranje19 <jasonwintery@gmail.com>
2020-05-12 17:36:36 -0700

    heroku

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ const passport = require('passport');
 
 const path = require('path');
 
+
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB Successfully"))
+.catch(err => console.log(err));
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"));
     app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-
-mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB Successfully"))
-    .catch(err => console.log(err));
-
 // app.get("/", (req, res) => res.send("Back to Hello World"));
 app.use(passport.initialize());
 require('./config/passport')(passport);
